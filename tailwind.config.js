af3ef24ef543171beb307311446e16e610a89f64
puenteformacion <puenteformacio@gmail.com>
2025-04-07 12:06:39 +0200

    primera subida

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default{
+  darkMode: 'class', // Habilita el modo oscuro usando la clase
+  //darkMode: ['selector', '[data-mode="dark"]'], // Habilita el modo oscuro usando las preferencias del sistema
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+}
